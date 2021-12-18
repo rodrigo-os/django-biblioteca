90017f5c0278d8f88e68d6dfeb7a0d858c28897c
Rodrigo Oliveira da Silva <rodrigoods49@gmail.com>
2021-12-18 05:24:32 -0300

    Página inicial

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from catalogo import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from catalogo.models import Genero, Linguagem, Autor, Livro, ExemplarLivro
+
+def index(request):
+
+    num_livros = Livro.objects.all().count()
+    num_exemplares = ExemplarLivro.objects.all().count()
+
+    num_exemplares_disponiveis = ExemplarLivro.objects.filter(situacao__exact='d').count()
+
+    num_autores = Autor. objects.count()
+
+    contexto = {
+        'num_livros': num_livros,
+        'num_exemplares': num_exemplares,
+        'num_exemplares_disponiveis': num_exemplares_disponiveis,
+        'num_autores': num_autores,
+    }
+
+    return render(request, 'index.html', context=contexto)
