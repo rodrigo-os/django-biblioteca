322002414ebaa2583bb04f91f5d9d7ce351e8338
Rodrigo Oliveira da Silva <rodrigoods49@gmail.com>
2021-12-18 04:40:11 -0300

    Modelos registrados no painel admin, fixture para testes

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from catalogo.models import Genero, Linguagem, Autor, Livro, ExemplarLivro
+
+admin.site.register(Genero)
+admin.site.register(Linguagem)
+admin.site.register(Autor)
+admin.site.register(Livro)
+admin.site.register(ExemplarLivro)
